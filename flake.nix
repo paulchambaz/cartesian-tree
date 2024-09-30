6b31cedfe565fcb7d56f4037b73f431e994e58d8
Paul Chambaz <paul.chambaz@tutanota.com>
2024-09-30 10:52:38 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+
+      buildPkgs = with pkgs; [
+      ];
+
+      libPkgs = with pkgs; [
+      ];
+
+      devPkgs = with pkgs; [
+        just
+      ];
+    in {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = buildPkgs;
+        buildInputs = libPkgs ++ devPkgs;
+      };
+    });
+}
