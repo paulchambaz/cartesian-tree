85b489d75fb34ff2e11a03df83a85cb49c47b68a
Paul Chambaz <paul.chambaz@tutanota.com>
2024-09-30 12:47:35 +0200

    finished 2.1

--- tme1/src/backpack.rs
+++ tme1/src/backpack.rs
@@ -1,17 +1,43 @@
+use rand::Rng;
+
 
 pub struct Backpack {
-    pub total_weight: u32,
-    pub weights: Vec<u32>,
-    pub utilities: Vec<u32>,
+    pub total_weight: u64,
+    pub weights: Vec<u64>,
+    pub utilities: Vec<u64>,
+}
+
+pub struct Solution {
+    pub utility: f64,
+    pub values: Vec<f64>,
 }
 
 
-pub fn greedy(backpack: Backpack) -> (u32, Vec<usize>) {
-    let mut ratios: Vec<(usize, f32)> = Vec::new();
+impl Backpack {
+    pub fn gen_random(r: usize) -> Backpack {
+        let mut rng = rand::thread_rng();
+        let weights: Vec<u64> = (0..r).map(|_| rng.gen_range(1..r) as u64).collect();
+        let utilities: Vec<u64> = (0..r).map(|_| rng.gen_range(1..r) as u64).collect();
+
+        let total_weight: u64 = weights.iter().sum::<u64>() / 2;
+
+        Backpack{total_weight, weights, utilities}
+    }
+}
+
+impl Solution {
+    pub fn print(&self) {
+        println!("Solution: {:?}, Utility: {}", self.values, self.utility);
+    }
+}
+
+
+pub fn greedy(backpack: &Backpack) -> Solution {
+    let mut ratios: Vec<(usize, f64)> = Vec::new();
     let len = backpack.weights.len();
 
     for i in 0..len {
-        let ratio = backpack.utilities[i] as f32 / backpack.weights[i] as f32;
+        let ratio = backpack.utilities[i] as f64 / backpack.weights[i] as f64;
         ratios.push((i, ratio));
     }
 
@@ -24,32 +50,128 @@ pub fn greedy(backpack: Backpack) -> (u32, Vec<usize>) {
         }
     });
 
-    let mut total_weight = backpack.total_weight;
-    let mut total_utility = 0;
-    let mut solution = Vec::new();
+    let mut remaining_weight = backpack.total_weight;
+    let mut total_utility = 0.0;
+    let mut solution = vec![0.0; len];
 
     for i in 0..len {
         let (index, _) = ratios[i];
         let weight = backpack.weights[index];
+        let utility = backpack.utilities[index];
 
-        if weight > total_weight {
+        if weight > remaining_weight {
             continue;
         }
 
-        let utility = backpack.weights[index];
 
-        total_weight -= weight;
-        total_utility += utility;
-        solution.push(index);
+        remaining_weight -= weight;
+        total_utility += utility as f64;
+        solution[index] = 1.0;
 
-        if total_weight <= 0 {
+        if remaining_weight <= 0 {
             break;
         }
     }
 
-    println!("{:?}", ratios);
-
-    (total_utility, solution)
+    Solution{utility: total_utility, values: solution}
 }
 
 
+pub fn greedy_continuous(backpack: &Backpack) -> Solution {
+    let mut ratios: Vec<(usize, f64)> = Vec::new();
+    let len = backpack.weights.len();
+
+    for i in 0..len {
+        let ratio = backpack.utilities[i] as f64 / backpack.weights[i] as f64;
+        ratios.push((i, ratio));
+    }
+
+
+    ratios.sort_by(|(_, ratio_i), (_, ratio_j)| {
+        if let Some(ordering) = ratio_i.partial_cmp(&ratio_j) {
+            ordering.reverse()
+        } else {
+            panic!("the variables are not comparable...");
+        }
+    });
+
+    let mut remaining_weight = backpack.total_weight;
+    let mut total_utility = 0.0;
+    let mut solution = vec![0.0; len];
+
+    for i in 0..len {
+        let (index, _) = ratios[i];
+        let weight = backpack.weights[index];
+        let utility = backpack.utilities[index];
+
+
+        if weight > remaining_weight {
+            let ratio = (remaining_weight as f64) / (weight as f64);
+            total_utility += ratio * utility as f64;
+            solution[index] = ratio;
+            break;
+        }
+
+        remaining_weight -= weight;
+        total_utility += utility as f64;
+        solution[index] = 1.0;
+
+        if remaining_weight <= 0 {
+            break;
+        }
+    }
+
+    Solution{utility: total_utility, values: solution}
+}
+
+pub fn bruteforce_treesearch(backpack: &Backpack) -> Option<Solution> {
+    if let Some((weight, solution)) = bruteforce_treesearch_runner(backpack, 0, vec![]) {
+        Some(solution)
+    } else {
+        None
+    }
+}
+
+pub fn bruteforce_treesearch_runner(backpack: &Backpack, i: usize, pos: Vec<usize>) -> Option<(u32, Solution)> {
+    let len = backpack.weights.len();
+    if i >= len {
+        let mut values: Vec<f64> = vec![0.0; len];
+        let mut utility = 0.0;
+        let mut weight = 0;
+        for p in pos {
+            values[p] = 1.0;
+            weight += backpack.weights[p];
+            utility += backpack.utilities[p] as f64;
+        }
+
+        if weight > backpack.total_weight {
+            return None
+        }
+
+        return Some((0, Solution{utility, values}));
+    }
+
+    let mut pos_clone = pos.clone();
+    pos_clone.push(i);
+
+    if let Some((weight_left, res_left)) = bruteforce_treesearch_runner(backpack, i + 1, pos_clone) {
+        if let Some((weight_right, res_right)) = bruteforce_treesearch_runner(backpack, i + 1, pos) {
+            if res_left.utility > res_right.utility {
+                Some((weight_left, res_left))
+            } else {
+                Some((weight_right, res_right))
+            }
+        } else {
+            Some((weight_left, res_left))
+        }
+    } else {
+        if let Some((weight_right, res_right)) = bruteforce_treesearch_runner(backpack, i + 1, pos) {
+            Some((weight_right, res_right))
+        } else {
+            None
+        }
+    }
+
+
+
+}
--- tme1/src/main.rs
+++ tme1/src/main.rs
@@ -1,26 +1,66 @@
-use backpack::{greedy, Backpack};
+use std::time::Instant;
+
+use backpack::{bruteforce_treesearch, greedy, greedy_continuous, Backpack};
+use gnuplot::{Figure, PlotOption::{Caption, Color}};
 
 mod backpack;
 
 fn main() {
-    println!("Hello, world!");
-
     // p13 - sujet td
     // p18 - sujet tme
 
-    let total_weight = 20;
-    let weights = vec![14, 10, 8, 6, 5, 2];
-    let utilities = vec![24, 19, 16, 13, 5, 3];
-    let backpack = Backpack{total_weight, weights, utilities};
+    test_example();
 
 
-    let (utility, solution) = greedy(backpack);
 
-    print!("solution: ");
-    for s in solution {
-        print!("{} ", s + 1);
+}
 
+pub fn test_example() {
+    let backpack = Backpack{
+        total_weight: 20,
+        weights: vec![14, 10, 8, 6, 5, 2],
+        utilities: vec![24, 19, 16, 13, 5, 3],
+    };
+    let solution = greedy(&backpack);
+    solution.print();
+    let solution = greedy_continuous(&backpack);
+    solution.print();
+    if let Some(solution) = bruteforce_treesearch(&backpack) {
+        solution.print();
     }
-    println!("utility: {}", utility);
-    // println!("solution: {:?} of utility: {}", solution, utility);
+}
+
+pub fn measure_performance() {
+    let sec_max = 0.1;
+    let mut tmax = 0;
+    for r in vec![100, 1_000, 10_000, 100_000, 1_000_000, 10_000_000] {
+        let backpack = Backpack::gen_random(r);
+        let start = Instant::now();
+        let _ = greedy(&backpack);
+        let duration = start.elapsed();
+
+        if duration.as_secs_f32() > sec_max {
+            tmax = r;
+            break;
+        }
+    }
+
+    let mut x = Vec::new();
+    let mut y = Vec::new();
+
+    for i in 1..=10 {
+        let t = i * tmax / 10;
+        let backpack = Backpack::gen_random(t);
+        let start = Instant::now();
+        let _ = greedy(&backpack);
+        let duration = start.elapsed();
+
+        x.push(t);
+        y.push(duration);
+    }
+
+    let mut fg = Figure::new();
+    fg.axes2d()
+        .lines(&x, &y, &[Caption("Greedy"), Color("black")]);
+    let _ = fg.show();
 }
